92023703e3a9e22a29bbc1be38c068747ce3ade8
glizabeth707 <142641327+glizabeth707@users.noreply.github.com>
2023-10-25 14:58:35 -0600

    wk 9

---
+++ Homework 9/sketch.js
@@ -0,0 +1,27 @@
+
+function setup()
+            {
+                createCanvas(500, 600);
+            }
+
+            function draw (draw)
+            {
+                fill('51');
+                background(120, 45, 78);
+                fill('rgba(100%,0%,100%,0.5)');
+                circle(250, 100, 100);
+                fill('#A251FA');
+                rect(200,160,100,150);
+                strokeWeight(10);
+                point(230, 100);
+                point(260, 100);
+                triangle(30, 75, 58, 20, 86, 75);
+                circle(246, 126, 20);
+                line(200, 170, 85, 75);
+                line(300, 170, 85, 175);
+                line(200, 300, 200, 400);
+                line(300, 400, 300, 300);
+                textSize(27);
+                text('Gwendelyn FC', 300, 430);
+            }
+            
