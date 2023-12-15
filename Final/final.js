610b38a83d011b3ed541668e389afc9557b0dd83
glizabeth707 <142641327+glizabeth707@users.noreply.github.com>
2023-12-15 16:08:14 -0700

    final assignment

---
+++ Final/final.js
@@ -0,0 +1,52 @@
+
+function setup()
+{
+    createCanvas(500, 600);
+}
+
+function draw()
+{
+    background('hsla(160, 100%, 50%, 0.5)');
+    
+
+    // head
+    fill(50, 55, 100);
+    circle(250,150,175);
+   
+ // head
+ fill('rgb(0,0,255)');
+ circle(150,300,175);
+
+ fill('rgb(0,0,255)');
+ circle(350,300,175);
+
+ fill(50, 55, 100);
+    circle(250,450,175);
+    
+fill(50, 55, 100);
+circle(100,450,50);
+
+fill(50, 55, 100);
+circle(400,150,50);
+
+fill('rgb(0,0,255)');
+ circle(100,150,50);
+
+ fill('rgb(0,0,255)');
+ circle(400,450,50);
+
+ fill(50, 55, 100);
+    circle(50,25,175);
+
+    fill('rgb(0,0,255)');
+    circle(450,25,175);
+
+    fill('rgb(0,0,255)');
+    circle(50,600,175);
+
+    fill(50, 55, 100);
+    circle(450,600,175);
+
+    fill(10, 24, 120);
+    rect(200,200,100,200);
+}
